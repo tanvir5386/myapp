f1fccf2e15fcc212d89cd465a4c30b50689e46ee
islam15-5386 <islam15-5386@diu.edu.bd>
2025-02-22 04:46:21 +0000

    new cached network

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(const Lab06AdvancedUI());
@@ -15,17 +16,15 @@ class Lab06AdvancedUI extends StatelessWidget {
         appBar: AppBar(title: const Text('Lab06 Advanced UI')),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.network(
-                'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial.jpg',
-              ),
-              Expanded(
-                child: Image(
-                  image: NetworkImage('https://picsum.photos/250?image=5'),
-                ),
-              ),
-              // New Image
-              Image.network('http://picsum.photos/250?image=10'),
+              CachedNetworkImage(
+                imageUrl: 'https://incubator.ucf.edu/wp-content/uploads/2023/07/artificial.jpg',
+                placeholder: (context, url) => const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ), // Cached Network Image
+              const SizedBox(height: 20),
+              Image.network('http://picsum.photos/250?image=10'), // Regular Network Image
             ],
           ),
         ),
